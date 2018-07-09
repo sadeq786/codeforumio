606f3d693975bfed8d73ecff6f726e60c83be33e
Sadeq Rahman <sadeq@MacBook-Pro-Sadeq-Rahman.local>
2018-07-09 02:04:23 -0500

    made POST mongoose model

---
+++ models/Post.js
@@ -0,0 +1,30 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema ({
+    postTitle: {
+        type: String,
+        required: true
+    },
+    stars: {
+        type: Number,
+        default: 0,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    comments : {
+        type: Array,
+        required: true
+    }
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
