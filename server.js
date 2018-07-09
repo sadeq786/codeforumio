a67f10c40782f0cbafd629e03d6b7d55bbfc04b9
Sadeq Rahman <sadeq@MacBook-Pro-Sadeq-Rahman.local>
2018-07-09 01:52:34 -0500

    connected to mongoose db

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// Configure body parser
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
+
+
 // Send every request to the React app
 // Define any API routes before this runs
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
+// Connect to Mongoose production mongo database
+mongoose.connect(process.env.MONGODB_URL || "mongodb://prod:prod123@ds213199.mlab.com:13199/heroku_7f7chzrm");
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
