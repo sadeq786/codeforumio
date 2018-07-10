83c0c2c5976407e65e0e701e1c5481d5aaf5ba2f
Sadeq Rahman <sadeq@MacBook-Pro-Sadeq-Rahman.local>
2018-07-10 12:15:54 -0500

    attempt 3: got it working

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Jumbotron.css"
+// import { BrowserRouter as Router, Route } from "react-router-dom";
+
 
 
 const Jumbotron = () => (
-
+    
     <div className="jumbotron">
         <h1 className="display-4">Welcome to CodeForum</h1>
         <p className="lead">
             <Link to="/all" className="btn btn-secondary btn-lg">Browse latest topics and posts</Link>
         </p>
     </div>
-
+    
 )
 
 export default Jumbotron;
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,16 +1,20 @@
 import React from "react";
-
-import { BrowserRouter as Router} from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jumbotron from "../components/Jumbotron";
+import All from "../pages/All";
 
 const Homepage = () => (
-    
-    <Router>
-        <div>
-            <Jumbotron />
-        </div>
-    </Router>
+    <div>
+        <Jumbotron />
+        <Router>
+            <div>
+                <Route exact path="/All" render={props => <All {...props} />}/>
+                {/* <Route exact path="/All" component={All} /> */}
+
+
+            </div>
+        </Router>
+    </div>
 )
 
 export default Homepage;
